8742f8fd71cbb260df4b204bc5f38aad27764f22
Eric <eric_gomez_1@hotmail.com>
2019-03-23 23:33:14 -0700

    captured the value being passed into the mock model entity and verify its contents.

--- src/test/java/app/recipes/controllers/IndexControllerTest.java
+++ src/test/java/app/recipes/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package app.recipes.controllers;
 
+import app.recipes.models.Recipe;
 import app.recipes.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest
 {
@@ -34,12 +38,30 @@ public class IndexControllerTest
     @Test
     public void getIndex()
     {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = controller.getIndex(model);
 
+        //then
         assertEquals("index", viewName);
 
         verify(recipeService, times(1)).getRecipes();
 
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }

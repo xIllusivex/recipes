460bf869d5ff5bb40b3232c077c26a900d10d2a6
Eric <eric_gomez_1@hotmail.com>
2019-03-25 09:00:07 -0700

    added a few tests to the UnitOfMeasureRepository

--- src/test/java/app/recipes/controllers/IndexControllerTest.java
+++ src/test/java/app/recipes/controllers/IndexControllerTest.java
@@ -7,15 +7,20 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest
 {
@@ -36,7 +41,18 @@ public class IndexControllerTest
     }
 
     @Test
-    public void getIndex()
+    public void testMockMVC() throws Exception
+    {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+    }
+
+    @Test
+    public void getIndex() throws Exception
     {
         //given
         Set<Recipe> recipes = new HashSet<>();

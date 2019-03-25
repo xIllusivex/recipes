ef127244e8ba49a6ea620fa28e3b10d52bc5c8c2
Eric <eric_gomez_1@hotmail.com>
2019-03-25 09:18:00 -0700

    added a new plugin which will run integration tests on the very build phase

--- src/test/java/app/recipes/repositorys/UnitOfMeasureRepositoryIT.java
+++ src/test/java/app/recipes/repositorys/UnitOfMeasureRepositoryIT.java
@@ -25,7 +25,7 @@ public class UnitOfMeasureRepositoryIT
     }
 
     @Test
-    public void findByDescriptionTeaspoon() throws Exception
+    public void findByDescription() throws Exception
     {
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 

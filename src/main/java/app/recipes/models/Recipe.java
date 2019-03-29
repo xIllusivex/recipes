cb70a78278167efbc72600931b41ce774878922b
Eric <eric_gomez_1@hotmail.com>
2019-03-29 14:21:07 -0700

    added a few changes to the recipe and ingredient models, implemented tests for the ingredient service. closes #6

--- src/main/java/app/recipes/models/Recipe.java
+++ src/main/java/app/recipes/models/Recipe.java
@@ -41,9 +41,11 @@ public class Recipe
     private Set<Category> categories = new HashSet<>();
 
 
-    public void addIngredient(Ingredient ingredient)
+    public Recipe addIngredient(Ingredient ingredient)
     {
-        ingredients.add(ingredient);
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public void setNotes(Notes notes)

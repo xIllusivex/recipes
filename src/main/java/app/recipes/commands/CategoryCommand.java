911e3cb75191b5254aaab08192d69f43af6ab75b
Eric <eric_gomez_1@hotmail.com>
2019-03-29 08:24:49 -0700

    added commands and converters

---
+++ src/main/java/app/recipes/commands/CategoryCommand.java
@@ -0,0 +1,14 @@
+package app.recipes.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryCommand
+{
+    private Long id;
+    private String description;
+}
---
+++ src/main/java/app/recipes/commands/UnitOfMeasureCommand.java
@@ -0,0 +1,15 @@
+package app.recipes.commands;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnitOfMeasureCommand
+{
+    private Long id;
+    private String description;
+}

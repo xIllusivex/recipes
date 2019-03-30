a6f5f05d1b4108ec1d2d6c6ef63bcb354e084fec
Eric <eric_gomez_1@hotmail.com>
2019-03-29 22:03:56 -0700

    added image controller, services, and views for handing the persistense and serving of image.

---
+++ src/main/java/app/recipes/controllers/ImageController.java
@@ -0,0 +1,66 @@
+package app.recipes.controllers;
+
+import app.recipes.commands.RecipeCommand;
+import app.recipes.services.ImageService;
+import app.recipes.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Controller
+public class ImageController
+{
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public ImageController(ImageService imageService, RecipeService recipeService)
+    {
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{id}/image")
+    public String serveForm(@PathVariable Long id, Model model)
+    {
+        model.addAttribute("recipe", recipeService.findCommandById(id));
+
+        return "recipe/imageUploadForm";
+    }
+
+    @GetMapping("/recipe/{id}/recipeimage")
+    public void serveImage(@PathVariable Long id, HttpServletResponse response) throws IOException
+    {
+        RecipeCommand recipeCommand = recipeService.findCommandById(id);
+
+        byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+        int i = 0;
+
+        for (Byte wrappedByte : recipeCommand.getImage())
+        {
+            byteArray[i++] = wrappedByte; // auto unboxing.
+        }
+
+        response.setContentType("image/jpeg");
+        InputStream is = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(is, response.getOutputStream());
+    }
+
+    @PostMapping("/recipe/{id}/image")
+    public String handleImagePost(@PathVariable Long id, @RequestParam("imagefile") MultipartFile file)
+    {
+        imageService.saveImage(id, file);
+
+        return "redirect:/recipe/show/" + id;
+    }
+}
---
+++ src/main/java/app/recipes/services/ImageServiceImpl.java
@@ -0,0 +1,53 @@
+package app.recipes.services;
+
+import app.recipes.models.Recipe;
+import app.recipes.repositorys.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.FileUploadBase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService
+{
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository)
+    {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    @Transactional
+    public void saveImage(Long recipeId, MultipartFile file)
+    {
+        log.debug("saving image.");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObject = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes())
+            {
+                byteObject[i++] = b;
+            }
+
+            recipe.setImage(byteObject);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e)
+        {
+            log.error("Error uploading file ", e);
+
+            e.printStackTrace();
+        }
+
+    }
+}
